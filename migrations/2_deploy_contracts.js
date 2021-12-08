da61cda4b3cc499306811fc916502f5874d4182d
KonsGio <88385243+KonsGio@users.noreply.github.com>
2021-12-08 16:35:40 +0200

    ethereum balance and token

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,11 @@
+const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
+
+module.exports = async function(deployer) {
+  await deployer.deploy(EthSwap);
+    const ethSwap = await EthSwap.deployed()
+  await deployer.deploy(Token);
+    const token = await Token.deployed()
+
+    await token.transfer(ethSwap.address, '1000000000000000000000000')
+};
